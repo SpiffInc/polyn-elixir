fa4169a26221e37584ad0e7dee55fa2cc37a5df5
brandynbennett <brandynlbennett@gmail.com>
2022-05-03 16:52:47 -0600

    docs on event

--- lib/core/event.ex
+++ lib/core/event.ex
@@ -1,8 +1,6 @@
 defmodule Polyn.Event do
   @moduledoc """
   The Event structure used throughout Polyn.
-
-
   """
   defstruct id: nil,
             specversion: nil,
@@ -17,6 +15,29 @@ defmodule Polyn.Event do
               langversion: System.build_info().version,
               version: Polyn.MixProject.version()
             }
+
+  @typedoc """
+  `id` - Identifies the event.
+  `specversion` - The version of the CloudEvents specification which the event uses.
+  `type` - Describes the type of event related to the originating occurrence.
+  `data` - The event payload.
+  `dataschema` - Identifies the schema that data adheres to.
+  `source` - Identifies the context in which an event happened.
+  `time` - Timestamp of when the occurrence happened. Must adhere to RFC 3339.
+  `polyntrace` - Previous events that led to this one
+  `polynclient` - Information about the client that produced the event
+  """
+  @type t() :: %__MODULE__{
+          id: String.t(),
+          specversion: String.t(),
+          type: String.t(),
+          data: any(),
+          dataschema: String.t(),
+          source: String.t(),
+          time: String.t(),
+          polyntrace: list(map()),
+          polynclient: map()
+        }
 
   def new(fields) when is_list(fields) do
     fields =
