7bf9d2e5914d48fad89c8f9170bb92dd9150c789
brandynbennett <brandynlbennett@gmail.com>
2022-05-13 17:01:56 -0600

    test calling `create_stream` twice

--- test/migration/migration_test.exs
+++ test/migration/migration_test.exs
@@ -29,4 +29,29 @@ defmodule Polyn.MigrationTest do
              data: %{name: "test_stream", subjects: ["foo"]}
            } = migration
   end
+
+  test "create_stream/2 called multiple works", %{runner: pid} do
+    create_stream(name: "test_stream", subjects: ["foo"])
+    create_stream(name: "test_stream2", subjects: ["bar"])
+
+    assert %{application_migrations: [migration1, migration2]} = LocalRunner.get_state(pid)
+
+    assert %Event{
+             id: "1234.1",
+             type: "com.test.polyn.stream.create.v1",
+             specversion: "1.0.1",
+             source: "com:test:my_app",
+             dataschema: "polyn:stream:create:schema:v1",
+             data: %{name: "test_stream", subjects: ["foo"]}
+           } = migration1
+
+    assert %Event{
+             id: "1234.2",
+             type: "com.test.polyn.stream.create.v1",
+             specversion: "1.0.1",
+             source: "com:test:my_app",
+             dataschema: "polyn:stream:create:schema:v1",
+             data: %{name: "test_stream2", subjects: ["bar"]}
+           } = migration2
+  end
 end
