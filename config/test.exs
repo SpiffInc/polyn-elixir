b878aab85a33f1869096050435c3aa4d445532e3
brandynbennett <brandynlbennett@gmail.com>
2022-05-13 14:47:06 -0600

    add domain to test config

--- config/test.exs
+++ config/test.exs
@@ -3,6 +3,8 @@ import Config
 config :polyn, :file, Polyn.FileMock
 config :polyn, :code, Polyn.CodeMock
 
+config :polyn, :domain, "com.test"
+
 config :polyn, :nats, %{
   name: :test_gnat,
   connection_settings: [
