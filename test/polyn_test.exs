f1273592d9ac505799d1f855f50dca93d206ef4f
brandynbennett <brandynlbennett@gmail.com>
2022-07-07 10:53:24 -0600

    put pub tests under describe block

--- test/polyn_test.exs
+++ test/polyn_test.exs
@@ -17,101 +17,103 @@ defmodule PolynTest do
     end)
   end
 
-  test "pub/3 adds a new event to the server" do
-    add_schema("pub.test.event.v1", %{
-      "type" => "object",
-      "properties" => %{"data" => %{"type" => "string"}}
-    })
+  describe "pub/4" do
+    test "adds a new event to the server" do
+      add_schema("pub.test.event.v1", %{
+        "type" => "object",
+        "properties" => %{"data" => %{"type" => "string"}}
+      })
 
-    Gnat.sub(@conn_name, self(), "pub.test.event.v1")
-    Polyn.pub(@conn_name, "pub.test.event.v1", "foo", store_name: @store_name)
+      Gnat.sub(@conn_name, self(), "pub.test.event.v1")
+      Polyn.pub(@conn_name, "pub.test.event.v1", "foo", store_name: @store_name)
 
-    data = get_message()
-    assert data["data"] == "foo"
-    assert data["datacontenttype"] == "application/json"
-    assert data["source"] == "com:test:user:backend"
-    assert data["specversion"] == "1.0.1"
-    assert data["type"] == "com.test.pub.test.event.v1"
-    assert data["polyntrace"] == []
-  end
+      data = get_message()
+      assert data["data"] == "foo"
+      assert data["datacontenttype"] == "application/json"
+      assert data["source"] == "com:test:user:backend"
+      assert data["specversion"] == "1.0.1"
+      assert data["type"] == "com.test.pub.test.event.v1"
+      assert data["polyntrace"] == []
+    end
 
-  test "pub/3 can include extra `source` info" do
-    add_schema("pub.test.event.v1", %{
-      "type" => "object",
-      "properties" => %{"data" => %{"type" => "string"}}
-    })
+    test "can include extra `source` info" do
+      add_schema("pub.test.event.v1", %{
+        "type" => "object",
+        "properties" => %{"data" => %{"type" => "string"}}
+      })
 
-    Gnat.sub(@conn_name, self(), "pub.test.event.v1")
-    Polyn.pub(@conn_name, "pub.test.event.v1", "foo", store_name: @store_name, source: "orders")
+      Gnat.sub(@conn_name, self(), "pub.test.event.v1")
+      Polyn.pub(@conn_name, "pub.test.event.v1", "foo", store_name: @store_name, source: "orders")
 
-    data = get_message()
-    assert data["source"] == "com:test:user:backend:orders"
-  end
+      data = get_message()
+      assert data["source"] == "com:test:user:backend:orders"
+    end
 
-  test "pub/3 builds up polyntrace if :triggered_by is supplied" do
-    add_schema("pub.test.event.v1", %{
-      "type" => "object",
-      "properties" => %{"data" => %{"type" => "string"}}
-    })
+    test "builds up polyntrace if :triggered_by is supplied" do
+      add_schema("pub.test.event.v1", %{
+        "type" => "object",
+        "properties" => %{"data" => %{"type" => "string"}}
+      })
 
-    Gnat.sub(@conn_name, self(), "pub.test.event.v1")
+      Gnat.sub(@conn_name, self(), "pub.test.event.v1")
 
-    trigger_event =
-      Event.new(
-        type: Event.full_type("user.form.finished.v1"),
-        polyntrace: [
-          %{
-            "id" => Event.new_event_id(),
-            "time" => Event.new_timestamp(),
-            "type" => Event.full_type("user.entered.site.v1")
-          }
-        ]
+      trigger_event =
+        Event.new(
+          type: Event.full_type("user.form.finished.v1"),
+          polyntrace: [
+            %{
+              "id" => Event.new_event_id(),
+              "time" => Event.new_timestamp(),
+              "type" => Event.full_type("user.entered.site.v1")
+            }
+          ]
+        )
+
+      Polyn.pub(@conn_name, "pub.test.event.v1", "foo",
+        store_name: @store_name,
+        triggered_by: trigger_event
       )
 
-    Polyn.pub(@conn_name, "pub.test.event.v1", "foo",
-      store_name: @store_name,
-      triggered_by: trigger_event
-    )
+      data = get_message()
 
-    data = get_message()
+      assert data["polyntrace"] == [
+               Enum.at(trigger_event.polyntrace, 0),
+               %{
+                 "id" => trigger_event.id,
+                 "time" => trigger_event.time,
+                 "type" => trigger_event.type
+               }
+             ]
+    end
 
-    assert data["polyntrace"] == [
-             Enum.at(trigger_event.polyntrace, 0),
-             %{
-               "id" => trigger_event.id,
-               "time" => trigger_event.time,
-               "type" => trigger_event.type
-             }
-           ]
-  end
+    test "raises if doesn't match schema" do
+      add_schema("pub.test.event.v1", %{
+        "type" => "object",
+        "properties" => %{"data" => %{"type" => "string"}}
+      })
 
-  test "pub/3 raises if doesn't match schema" do
-    add_schema("pub.test.event.v1", %{
-      "type" => "object",
-      "properties" => %{"data" => %{"type" => "string"}}
-    })
+      assert_raise(Polyn.ValidationException, fn ->
+        Polyn.pub(@conn_name, "pub.test.event.v1", 100, store_name: @store_name, source: "orders")
+      end)
+    end
 
-    assert_raise(Polyn.ValidationException, fn ->
-      Polyn.pub(@conn_name, "pub.test.event.v1", 100, store_name: @store_name, source: "orders")
-    end)
-  end
+    test "passes through other options" do
+      add_schema("pub.test.event.v1", %{
+        "type" => "object",
+        "properties" => %{"data" => %{"type" => "string"}}
+      })
 
-  test "pub/3 passes through other options" do
-    add_schema("pub.test.event.v1", %{
-      "type" => "object",
-      "properties" => %{"data" => %{"type" => "string"}}
-    })
+      Gnat.sub(@conn_name, self(), "pub.test.event.v1")
+      Polyn.pub(@conn_name, "pub.test.event.v1", "foo", store_name: @store_name, reply_to: "foo")
 
-    Gnat.sub(@conn_name, self(), "pub.test.event.v1")
-    Polyn.pub(@conn_name, "pub.test.event.v1", "foo", store_name: @store_name, reply_to: "foo")
+      reply_to =
+        receive do
+          {:msg, %{reply_to: reply_to}} ->
+            reply_to
+        end
 
-    reply_to =
-      receive do
-        {:msg, %{reply_to: reply_to}} ->
-          reply_to
-      end
-
-    assert reply_to == "foo"
+      assert reply_to == "foo"
+    end
   end
 
   defp add_schema(type, schema) do
