9243d4c9a84f1ce4bd7cd811e7f4b19fe4510f18
brandynbennett <brandynlbennett@gmail.com>
2022-09-12 15:35:28 -0600

    pull consumer uses genserver store

--- test/core/pull_consumer_test.exs
+++ test/core/pull_consumer_test.exs
@@ -36,21 +36,29 @@ defmodule Polyn.PullConsumerTest do
   end
 
   setup do
-    SchemaStore.create_store(@conn_name, name: @store_name)
-
-    add_schema("pull.consumer.test.event.v1", %{
-      "type" => "object",
-      "required" => ["type"],
-      "properties" => %{
-        "data" => %{
-          "type" => "object",
-          "properties" => %{
-            "name" => %{"type" => "string"},
-            "element" => %{"type" => "string"}
-          }
-        }
-      }
-    })
+    start_supervised!(
+      {SchemaStore,
+       [
+         store_name: @store_name,
+         connection_name: :foo,
+         schemas: %{
+           "pull.consumer.test.event.v1" =>
+             Jason.encode!(%{
+               "type" => "object",
+               "required" => ["type"],
+               "properties" => %{
+                 "data" => %{
+                   "type" => "object",
+                   "properties" => %{
+                     "name" => %{"type" => "string"},
+                     "element" => %{"type" => "string"}
+                   }
+                 }
+               }
+             })
+         }
+       ]}
+    )
 
     setup_stream()
     setup_consumer()
@@ -233,7 +241,6 @@ defmodule Polyn.PullConsumerTest do
     # Manage connection on our own here, because all supervised processes will be
     # closed by the time `on_exit` runs
     {:ok, pid} = Gnat.start_link()
-    :ok = SchemaStore.delete_store(pid, name: @store_name)
     :ok = Consumer.delete(pid, @stream_name, @consumer_name)
     :ok = Stream.delete(pid, @stream_name)
     Gnat.stop(pid)
