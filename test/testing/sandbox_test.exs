c2f0e0ed557a643a98be11bde071326aa12a44b4
brandynbennett <brandynlbennett@gmail.com>
2022-09-14 17:29:48 -0600

    Sandbox can setup allowances

--- test/testing/sandbox_test.exs
+++ test/testing/sandbox_test.exs
@@ -12,15 +12,36 @@ defmodule Polyn.SandboxTest do
   test "Can setup map a pid to nats pid" do
     start_supervised!(Sandbox)
 
-    assert Sandbox.setup_test(:foo, :bar)
-    assert Sandbox.state() == %{foo: :bar}
+    assert :ok = Sandbox.setup_test(:foo, :bar)
+    assert Sandbox.state() == %{foo: %{nats: :bar}}
   end
 
   test "Can delete a pid" do
     start_supervised!(Sandbox)
 
-    assert Sandbox.setup_test(:foo, :bar)
-    assert Sandbox.teardown_test(:foo)
+    assert :ok = Sandbox.setup_test(:foo, :bar)
+    assert :ok = Sandbox.teardown_test(:foo)
     assert Sandbox.state() == %{}
+  end
+
+  describe "allow/2" do
+    test "Adds a new allowance" do
+      start_supervised!(Sandbox)
+
+      assert :ok = Sandbox.setup_test(:foo, :bar)
+      assert :ok = Sandbox.allow(:foo, :other_pid)
+      assert Sandbox.get(:other_pid) == %{nats: :bar, allowed_by: [:foo]}
+    end
+
+    test "Raises if multiple tests allowing same shared process" do
+      Process.flag(:trap_exit, true)
+      start_supervised!(Sandbox)
+
+      assert :ok = Sandbox.setup_test(:test1, :nats1)
+      assert :ok = Sandbox.setup_test(:test2, :nats2)
+      assert :ok = Sandbox.allow(:test1, :other_pid)
+
+      catch_exit(Sandbox.allow(:test2, :other_pid))
+    end
   end
 end
