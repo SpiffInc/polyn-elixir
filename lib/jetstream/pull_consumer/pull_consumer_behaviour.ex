9808adb19e2520044dc7040c6975d4a3e114d0b0
brandynbennett <brandynlbennett@gmail.com>
2022-09-21 12:58:01 -0600

    turn off doc on pull consumer behaviour

--- lib/jetstream/pull_consumer/pull_consumer_behaviour.ex
+++ lib/jetstream/pull_consumer/pull_consumer_behaviour.ex
@@ -1,4 +1,6 @@
 defmodule Polyn.Jetstream.PullConsumerBehaviour do
+  @moduledoc false
+
   @callback start_link(module(), init_arg :: term(), options :: GenServer.options()) ::
               GenServer.on_start()
   @callback start(module(), init_arg :: term(), options :: GenServer.options()) ::
