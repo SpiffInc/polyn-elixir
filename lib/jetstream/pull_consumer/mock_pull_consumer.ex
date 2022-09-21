9aadb3762360993f176d250e220f3374a82b5b2e
brandynbennett <brandynlbennett@gmail.com>
2022-09-21 15:58:44 -0600

    add subjects_for_consumer to jetstream wrapper

--- lib/jetstream/pull_consumer/mock_pull_consumer.ex
+++ lib/jetstream/pull_consumer/mock_pull_consumer.ex
@@ -29,8 +29,14 @@ defmodule Polyn.Jetstream.MockPullConsumer do
   def init({module, init_arg}) do
     case module.init(init_arg) do
       {:ok, state, conn_opts} ->
-        lookup_stream!(conn_opts)
-        |> find_consumer_subjects(lookup_consumer!(conn_opts))
+        # This relies on a real look up on a running nats server.
+        # This ensures that applications are using real consumer
+        # names and real stream names that exist in their server
+        Polyn.Jetstream.subjects_for_consumer(
+          Keyword.fetch!(conn_opts, :connection_name),
+          Keyword.fetch!(conn_opts, :stream_name),
+          Keyword.fetch!(conn_opts, :consumer_name)
+        )
         |> subscribe_to_subjects()
 
         {:ok,
@@ -42,34 +48,6 @@ defmodule Polyn.Jetstream.MockPullConsumer do
 
       other ->
         other
-    end
-  end
-
-  defp lookup_stream!(conn_opts) do
-    Polyn.Jetstream.stream_info!(
-      Keyword.fetch!(conn_opts, :connection_name),
-      Keyword.fetch!(conn_opts, :stream_name)
-    )
-  end
-
-  # This relies on a real look up on a running nats server.
-  # This ensures that applications are using real consumer
-  # names and real stream names that exist in their server
-  defp lookup_consumer!(conn_opts) do
-    Polyn.Jetstream.consumer_info!(
-      Keyword.fetch!(conn_opts, :connection_name),
-      Keyword.fetch!(conn_opts, :stream_name),
-      Keyword.fetch!(conn_opts, :consumer_name)
-    )
-  end
-
-  defp find_consumer_subjects(stream, consumer) do
-    stream_subjects = stream.config.subjects
-    consumer_subject = consumer.config.filter_subject
-
-    case consumer_subject do
-      nil -> stream_subjects
-      subject -> [subject]
     end
   end
 
