540afd0bbf418a39361779ad773694d039c5deaf
brandynbennett <brandynlbennett@gmail.com>
2022-09-22 16:54:46 -0600

    mock acknowledger

---
+++ lib/jetstream/producer/jetstream_acknowledger.ex
@@ -0,0 +1,12 @@
+defmodule Polyn.Jetstream.Acknowledger do
+  # Real calls to Offbroadway.Jetstream.Acknowledger
+  @moduledoc false
+
+  @behaviour Broadway.Acknowledger
+
+  @impl Broadway.Acknowledger
+  defdelegate ack(ack_ref, successful, failed), to: OffBroadway.Jetstream.Acknowledger
+
+  @impl Broadway.Acknowledger
+  defdelegate configure(ack_ref, ack_data, options), to: OffBroadway.Jetstream.Acknowledger
+end
