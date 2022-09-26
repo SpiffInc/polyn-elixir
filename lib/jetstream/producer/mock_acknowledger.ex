315a899ed433900e98b95e99b9c3d9c3e121c9d4
brandynbennett <brandynlbennett@gmail.com>
2022-09-26 11:03:46 -0600

    keep broadway optional

--- lib/jetstream/producer/mock_acknowledger.ex
+++ lib/jetstream/producer/mock_acknowledger.ex
@@ -1,16 +1,18 @@
-defmodule Polyn.Jetstream.MockAcknowledger do
-  # Mock calls to Offbroadway.Jetstream.Acknowledger for testing isolation
-  @moduledoc false
+with {:module, _} <- Code.ensure_compiled(Broadway) do
+  defmodule Polyn.Jetstream.MockAcknowledger do
+    # Mock calls to Offbroadway.Jetstream.Acknowledger for testing isolation
+    @moduledoc false
 
-  @behaviour Broadway.Acknowledger
+    @behaviour Broadway.Acknowledger
 
-  @impl Broadway.Acknowledger
-  def ack(_ack_ref, _successful, _failed) do
-    :ok
-  end
+    @impl Broadway.Acknowledger
+    def ack(_ack_ref, _successful, _failed) do
+      :ok
+    end
 
-  @impl Broadway.Acknowledger
-  def configure(_ack_ref, ack_data, _options) do
-    {:ok, ack_data}
+    @impl Broadway.Acknowledger
+    def configure(_ack_ref, ack_data, _options) do
+      {:ok, ack_data}
+    end
   end
 end
