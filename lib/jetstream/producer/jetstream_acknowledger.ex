01aa6f140c28aaef8dc397d73122950546b9afc1
brandynbennett <brandynlbennett@gmail.com>
2022-09-26 11:03:46 -0600

    keep broadway optional

--- lib/jetstream/producer/jetstream_acknowledger.ex
+++ lib/jetstream/producer/jetstream_acknowledger.ex
@@ -1,12 +1,14 @@
-defmodule Polyn.Jetstream.Acknowledger do
-  # Real calls to Offbroadway.Jetstream.Acknowledger
-  @moduledoc false
+with {:module, _} <- Code.ensure_compiled(Broadway) do
+  defmodule Polyn.Jetstream.Acknowledger do
+    # Real calls to Offbroadway.Jetstream.Acknowledger
+    @moduledoc false
 
-  @behaviour Broadway.Acknowledger
+    @behaviour Broadway.Acknowledger
 
-  @impl Broadway.Acknowledger
-  defdelegate ack(ack_ref, successful, failed), to: OffBroadway.Jetstream.Acknowledger
+    @impl Broadway.Acknowledger
+    defdelegate ack(ack_ref, successful, failed), to: OffBroadway.Jetstream.Acknowledger
 
-  @impl Broadway.Acknowledger
-  defdelegate configure(ack_ref, ack_data, options), to: OffBroadway.Jetstream.Acknowledger
+    @impl Broadway.Acknowledger
+    defdelegate configure(ack_ref, ack_data, options), to: OffBroadway.Jetstream.Acknowledger
+  end
 end
--- lib/jetstream/producer/mock_producer.ex
+++ lib/jetstream/producer/mock_producer.ex
@@ -1,82 +1,84 @@
-defmodule Polyn.Jetstream.MockProducer do
-  # Mock calls to Offbroadway.Jetstream.Producer for testing isolation
-  @moduledoc false
+with {:module, _} <- Code.ensure_compiled(Broadway) do
+  defmodule Polyn.Jetstream.MockProducer do
+    # Mock calls to Offbroadway.Jetstream.Producer for testing isolation
+    @moduledoc false
 
-  use GenStage
+    use GenStage
 
-  alias Broadway.Message
-  alias Polyn.MockNats
+    alias Broadway.Message
+    alias Polyn.MockNats
 
-  @behaviour Broadway.Producer
+    @behaviour Broadway.Producer
 
-  @impl GenStage
-  def init(opts \\ []) do
-    subjects =
-      Polyn.Jetstream.subjects_for_consumer(
-        Keyword.fetch!(opts, :connection_name),
-        Keyword.fetch!(opts, :stream_name),
-        Keyword.fetch!(opts, :consumer_name)
-      )
+    @impl GenStage
+    def init(opts \\ []) do
+      subjects =
+        Polyn.Jetstream.subjects_for_consumer(
+          Keyword.fetch!(opts, :connection_name),
+          Keyword.fetch!(opts, :stream_name),
+          Keyword.fetch!(opts, :consumer_name)
+        )
 
-    subscribe_to_subjects(subjects)
+      subscribe_to_subjects(subjects)
 
-    messages = fetch_all_messages(subjects)
+      messages = fetch_all_messages(subjects)
 
-    {:producer, %{demand: 0, subjects: subjects, messages: messages, ack_ref: nil}}
-  end
+      {:producer, %{demand: 0, subjects: subjects, messages: messages, ack_ref: nil}}
+    end
 
-  @impl GenStage
-  def handle_demand(incoming_demand, %{demand: demand} = state) do
-    load_messages(%{state | demand: demand + incoming_demand})
-  end
+    @impl GenStage
+    def handle_demand(incoming_demand, %{demand: demand} = state) do
+      load_messages(%{state | demand: demand + incoming_demand})
+    end
 
-  @impl Broadway.Producer
-  def prepare_for_start(_module, broadway_opts) do
-    {[], broadway_opts}
-  end
+    @impl Broadway.Producer
+    def prepare_for_start(_module, broadway_opts) do
+      {[], broadway_opts}
+    end
 
-  @impl Broadway.Producer
-  def prepare_for_draining(state) do
-    {:noreply, [], state}
-  end
+    @impl Broadway.Producer
+    def prepare_for_draining(state) do
+      {:noreply, [], state}
+    end
 
-  defp load_messages(%{demand: demand} = state) when demand > 0 do
-    {to_send, remaining} = Enum.split(state.messages, demand)
+    defp load_messages(%{demand: demand} = state) when demand > 0 do
+      {to_send, remaining} = Enum.split(state.messages, demand)
 
-    {:noreply, to_send, %{state | messages: remaining}}
-  end
+      {:noreply, to_send, %{state | messages: remaining}}
+    end
 
-  defp load_messages(state) do
-    {:noreply, [], state}
-  end
+    defp load_messages(state) do
+      {:noreply, [], state}
+    end
 
-  defp fetch_all_messages(subjects) do
-    MockNats.get_messages()
-    |> Enum.filter(fn msg ->
-      Enum.any?(subjects, &Polyn.Naming.subject_matches?(msg.topic, &1))
-    end)
-    |> Enum.map(&wrap_message/1)
-  end
+    defp fetch_all_messages(subjects) do
+      MockNats.get_messages()
+      |> Enum.filter(fn msg ->
+        Enum.any?(subjects, &Polyn.Naming.subject_matches?(msg.topic, &1))
+      end)
+      |> Enum.map(&wrap_message/1)
+    end
 
-  defp wrap_message(msg) do
-    %Message{
-      data: msg.body,
-      metadata: %{
-        topic: msg.topic
-      },
-      acknowledger: {Polyn.Jetstream.MockAcknowledger, nil, msg.body}
-    }
-  end
+    defp wrap_message(msg) do
+      %Message{
+        data: msg.body,
+        metadata: %{
+          topic: msg.topic
+        },
+        acknowledger: {Polyn.Jetstream.MockAcknowledger, nil, msg.body}
+      }
+    end
 
-  defp subscribe_to_subjects(subjects) do
-    Enum.each(subjects, fn subject ->
-      Polyn.MockNats.sub(:foo, self(), subject)
-    end)
-  end
+    defp subscribe_to_subjects(subjects) do
+      Enum.each(subjects, fn subject ->
+        Polyn.MockNats.sub(:foo, self(), subject)
+      end)
+    end
 
-  # When a new message is published we'll add it to the list of messages
-  @impl GenStage
-  def handle_info({:msg, msg}, state) do
-    load_messages(%{state | messages: state.messages ++ [wrap_message(msg)]})
+    # When a new message is published we'll add it to the list of messages
+    @impl GenStage
+    def handle_info({:msg, msg}, state) do
+      load_messages(%{state | messages: state.messages ++ [wrap_message(msg)]})
+    end
   end
 end
